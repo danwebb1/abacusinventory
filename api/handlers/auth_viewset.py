9c1a3c867a36615dfeea19355108f8f48f3925eb
Dan Webb <dan.webb8706@gmail.com>
2020-10-27 20:03:55 -0500

    add method to create inventory item upon account creation

--- api/handlers/auth_viewset.py
+++ api/handlers/auth_viewset.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+import datetime
 import typing
 
+from api.models.inventory import Supply, Item
+from api.utils import get_user_id_from_request
 from ..definitions import GET, POST
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -19,6 +22,19 @@ class AuthViewSet(viewsets.ViewSet):
         if portal:
             new_user = User(portal_firestore_key=portal)
             new_user.save()
+            return Response(status=200)
+        else:
+            return HttpResponse(status=401)
+
+    @action(methods=[POST], detail=False)
+    def create_inventory(self, request: Request):
+
+        user = get_user_id_from_request(request)
+        if user:
+            item = Item(id=0)
+            user = User(id=user)
+            new_inventory = Supply(user_id=user, item_id=item, amount=0, date=datetime.datetime.now())
+            new_inventory.save()
             return Response(status=200)
         else:
             return HttpResponse(status=401)
