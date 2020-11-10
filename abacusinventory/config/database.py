93ff5e2524afc8aef16025e89377a4ebcae12243
Dan Webb <dan.webb8706@gmail.com>
2020-11-09 22:28:26 -0600

    update mysql host, added unix socket

--- abacusinventory/config/database.py
+++ abacusinventory/config/database.py
@@ -4,7 +4,8 @@ GAE_PROJECT_ID = 'abacus-app'
 SQL_ENGINE = 'django.db.backends.mysql'
 
 #SQL_HOST_DEV = '34.66.101.207'
-SQL_HOST_DEV = '/cloudsql/abacus-app-284522:us-central1:abacus-inventory'
+SQL_HOST_DEV = '127.0.0.1'
+SQL_UNIX_SOCKET = 'abacus-app-284522:us-central1:abacus-inventory'
 #SQL_HOST_QA
 #SQL_HOST_STAGING
 #SQL_HOST_PROD
@@ -17,5 +18,6 @@ DATABASE_DEV = {
     'USER': SQL_USER,
     'PASSWORD': SQL_PW,
     'NAME': SQL_NAME,
-    'GAE_PROJECT_ID': GAE_PROJECT_ID
+    'GAE_PROJECT_ID': GAE_PROJECT_ID,
+    'UNIX_SOCKET': SQL_UNIX_SOCKET
 }
